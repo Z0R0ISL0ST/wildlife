e868caee5fd4e4bf1e7176efc576b5cb6e2f1572
Suprith K <suprithsuppy0230@gmail.com>
2025-02-16 16:03:05 +0530

    Update app4.py

--- app4.py
+++ app4.py
@@ -25,7 +25,7 @@ uploaded_file = st.file_uploader("Upload a footprint image", type=["jpg", "jpg",
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     # Preprocess the image
     image = np.array(image)
